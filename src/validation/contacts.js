82e563c885a13d889cef738f5991b3bf96682ab5
Oleh-Liubimov <oleh.liubimov@gmail.com>
2024-08-13 19:44:45 +0300

    1.0

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -6,7 +6,7 @@ export const createContactSchema = Joi.object({
   email: Joi.string().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
-  userId: Joi.string(),
+  userId: Joi.string().required(),
   photo: Joi.string(),
 });
 
