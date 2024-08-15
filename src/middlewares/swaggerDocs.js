bdefcfba64ba2ffa59197493cd29874a128171d8
Oleh-Liubimov <oleh.liubimov@gmail.com>
2024-08-15 09:01:31 +0300

    swagger-docs+

---
+++ src/middlewares/swaggerDocs.js
@@ -0,0 +1,16 @@
+import createHttpError from 'http-errors';
+import swaggerUI from 'swagger-ui-express';
+import fs from 'node:fs';
+
+import { SWAGGER_PATH } from '../constants/index.js';
+
+export const swaggerDocs = () => {
+  try {
+    const swaggerDocs = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    return [...swaggerUI.serve, swaggerUI.setup(swaggerDocs)];
+  } catch (error) {
+    return (req, res, next) => {
+      next(createHttpError(500, 'Can`t load swagger docs'));
+    };
+  }
+};
